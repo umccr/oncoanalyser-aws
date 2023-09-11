8b4fb1c4b281c2f5f0d9b8c09aa9266957474500
Stephen Watts <scwatts@users.noreply.github.com>
2023-09-12 09:50:58 +1000

    Bump oncoanalyser version: 0.1.0 → 0.1.1 (#48)

--- application/settings.ts
+++ application/settings.ts
@@ -55,7 +55,7 @@ export class StarAlignNf extends Shared {
 
 export class Oncoanalyser extends Shared {
 
-  readonly versionTag = "v0.1.0";
+  readonly versionTag = "v0.1.1";
 
   getS3Data() {
     return new Map<string, string>([
