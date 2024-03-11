de29068549ae17df47bca41930e7d16ce2c6f6a4
Stephen Watts <scwatts@users.noreply.github.com>
2024-03-11 11:44:39 +1100

    Bump sash: 0.4.10 (#87)

--- application/settings.ts
+++ application/settings.ts
@@ -90,7 +90,7 @@ export class Oncoanalyser extends Shared {
 
 
 export class Sash extends Shared {
-  readonly versionTag = 'v0.4.5';
+  readonly versionTag = 'v0.4.10'
 
   getSsmParameters() {
     return new Map<string, string>([
