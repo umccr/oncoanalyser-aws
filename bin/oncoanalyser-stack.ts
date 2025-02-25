80c4b96813d98281f1cc72dbb46b38c901fff857
Stephen Watts <hello@stephen.ac>
2025-02-25 20:19:26 +1100

    wip

--- bin/oncoanalyser-stack.ts
+++ bin/oncoanalyser-stack.ts
@@ -5,7 +5,7 @@ import * as settings from '../settings';
 import { ApplicationStack } from '../lib/application-stack';
 
 const app = new cdk.App()
-const stack = new ApplicationStack(app, 'ApplicationStack', {
+const stack = new ApplicationStack(app, 'OncoanalyserStack', {
   env: {
     account: settings.AWS_ACCOUNT,
     region: settings.AWS_REGION,
