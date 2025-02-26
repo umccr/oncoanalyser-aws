07c93b66083470ad1794717876f10350e27a7b0b
qclayssen <quentin.clayssen@gmail.com>
2025-02-26 16:11:56 +1100

    Comment out ebs-autoscalling policie for task

--- lib/application-stack.ts
+++ lib/application-stack.ts
@@ -46,22 +46,23 @@ export class ApplicationStack extends cdk.Stack {
         iam.ManagedPolicy.fromAwsManagedPolicyName('service-role/AmazonEC2ContainerServiceforEC2Role'),
       ],
     });
-
-    new iam.Policy(this, 'TaskEbsAutoscalePolicy', {
-      roles: [roleBatchInstanceTask],
-      statements: [new iam.PolicyStatement({
-        actions: [
-          'ec2:CreateVolume',
-          'ec2:AttachVolume',
-          'ec2:DeleteVolume',
-          'ec2:DescribeVolumes',
-          'ec2:DescribeVolumeStatus',
-          'ec2:ModifyInstanceAttribute',
-          'ec2:CreateTags'
-        ],
-        resources: ['*']
-      })],
-    });
+  
+    // We may need this later for ebs-autoscalling purposes
+    // new iam.Policy(this, 'TaskEbsAutoscalePolicy', {
+      // roles: [roleBatchInstanceTask],
+      // statements: [new iam.PolicyStatement({
+        // actions: [
+          // 'ec2:CreateVolume',
+          // 'ec2:AttachVolume',
+          // 'ec2:DeleteVolume',
+          // 'ec2:DescribeVolumes',
+          // 'ec2:DescribeVolumeStatus',
+          // 'ec2:ModifyInstanceAttribute',
+          // 'ec2:CreateTags'
+        // ],
+        // resources: ['*']
+      // })],
+    // });
 
     const launchTemplate = this.getLaunchTemplate({
       securityGroup: securityGroup,
