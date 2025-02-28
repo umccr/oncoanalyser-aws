cfec77330ba7a4c8a8e3803261bf39bcf01f01cf
andrewpatto <andrew@patto.net>
2025-02-28 15:36:08 +1100

    Brought some settings up to CDK top level Removed all fixed names not externally referenced

--- bin/settings.ts
+++ bin/settings.ts
@@ -12,15 +12,19 @@ export const SETTINGS: OncoanalyserProps = {
     refDataPrefix: "refdata",
   },
   // NOTE(SW): temporary reduction for testing during dev
-  //maxPipelineCpus: 64,
-  //maxTaskCpus: 256,
-  maxPipelineCpus: 2,
-  maxTaskCpus: 4,
+  //pipelineMaxCpus: 64,
+  //taskMaxCpus: 256,
+  pipelineMaxCpus: 2,
+  taskMaxCpus: 4,
   pipelineInstanceTypes: [
     ec2.InstanceType.of(ec2.InstanceClass.R6A, ec2.InstanceSize.LARGE),
   ],
+  pipelineQueueName:  "oncoanalyser-pipeline",
+  pipelineJobDefinitionName: "oncoanalyser-job-definition",
   taskInstanceTypes: [
     ec2.InstanceType.of(ec2.InstanceClass.R6I, ec2.InstanceSize.XLARGE),
   ],
   vpc: undefined,
+  gitRepo: "https://github.com/scwatts/oncoanalyser-aws-stack-testing",
+  gitBranch: "aws-stack-testing"
 };
